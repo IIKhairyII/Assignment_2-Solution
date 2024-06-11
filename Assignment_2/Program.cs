886c4b6aae752a147970efe822164403873fe599
IIKhairy-TildeII <k.khairy@tildetech.ae>
2024-06-11 16:01:00 +0300

    5 problems finished

--- Assignment_2/Program.cs
+++ Assignment_2/Program.cs
@@ -5,19 +5,33 @@
         static void Main(string[] args)
         {
             #region Problem_1
-            Console.WriteLine("Please enter any Number:");
-            int number;
-            if (int.TryParse(Console.ReadLine(), out number))
-                Console.WriteLine($"You have entered:  {number}");
-            else
-                Console.WriteLine($"Make sure to enter a valid number");
+            //Console.WriteLine("Please enter any Number:");
+            //int number;
+            //if (int.TryParse(Console.ReadLine(), out number))
+            //    Console.WriteLine($"You have entered:  {number}");
+            //else
+            //    Console.WriteLine($"Make sure to enter a valid number");
             #endregion
             #region Problem_2
-            string toBeConverted = "v50";
-            Convert.ToInt32(toBeConverted); //Exception will be thrown if string is empty or contains non-numeric char 
+            //string toBeConverted = "v50";
+            //Convert.ToInt32(toBeConverted); //Exception will be thrown if string is empty or contains non-numeric char 
             #endregion
-
-
+            #region Problem_3
+            //float num1 = 5.5f;
+            //float num2 = 6.9f;
+            //float result = num1 + num2;
+            //Console.WriteLine(result); 
+            #endregion
+            #region Problem_4
+            //string sentence = "Hello world! This is the first assignment in C#";
+            //Console.WriteLine(sentence.Substring(0, 13)); 
+            #endregion
+            #region Problem_5
+            int x = 5, y = 7;
+            y = x;
+            x++;
+            Console.WriteLine($"x= {x} , y = {y}"); //The value of x changes but y is not affected anymore by the value of x as we passed only value not reference 
+            #endregion
         }
     }
 }
