ad30b33a701bbc3a7d46e3fd5345a7782ff44453
IIKhairy-TildeII <k.khairy@tildetech.ae>
2024-06-11 15:51:33 +0300

    Add project files.

---
+++ Assignment_2/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Assignment_2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Problem_1
+            Console.WriteLine("Please enter any Number:");
+            int number;
+            if (int.TryParse(Console.ReadLine(), out number))
+                Console.WriteLine($"You have entered:  {number}");
+            else
+                Console.WriteLine($"Make sure to enter a valid number");
+            #endregion
+            #region Problem_2
+            string toBeConverted = "v50";
+            Convert.ToInt32(toBeConverted); //Exception will be thrown if string is empty or contains non-numeric char 
+            #endregion
+
+
+        }
+    }
+}
