c9861d1377fb87bb1444fecacf23dd113ed4da4f
IIKhairy-TildeII <k.khairy@tildetech.ae>
2024-06-12 10:09:38 +0300

    Demo Assignment

---
+++ Demo/Point.cs
@@ -0,0 +1,10 @@
+﻿namespace Demo
+{
+    public class Point
+    {
+        public int x;
+        public int y;
+        public override string ToString() => $"x = {x},  y={y}";
+
+    }
+}
--- Demo/Program.cs
+++ Demo/Program.cs
@@ -4,7 +4,39 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region ValueType
+            //int x;
+            //x = 4;
+            //Int32 y;
+            //y = x;
+            //x++;
+            //Console.WriteLine(x + "   " + y);
+            #endregion
+            #region ReferenceType
+            //Point P1;
+            //P1 = new();
+            //Point P2 = new();
+            //P2 = P1;
+            //P1.x = 9;
+            //Console.WriteLine(P2.x);
+            //Console.WriteLine(P2.ToString());
+            #endregion
+            #region Object
+            //object o1;
+            //o1 = "o1 is now a string";
+            //Console.WriteLine(o1);
+            //o1 = 2;
+            //Console.WriteLine(o1);
+            //o1 = false;
+            //Console.WriteLine(o1); 
+            #endregion
+            #region Parsing
+            Console.WriteLine("Enter a number:");
+            int num = int.Parse(Console.ReadLine());
+            int num_2 = Convert.ToInt32(num.ToString());
+            bool flag = Convert.ToBoolean("FALSE");
+            Console.WriteLine(flag); 
+            #endregion
         }
     }
 }
